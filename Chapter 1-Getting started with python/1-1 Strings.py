7927d7e93a4ea8d31cce159c8061973fac4e0fd5
ibraahimmm <119839270+ibraahimmm@users.noreply.github.com>
2023-01-07 14:05:23 +0400

    Add files via upload

---
+++ Chapter 1-Getting started with python/1-1 Strings.py
@@ -0,0 +1,20 @@
+
+a="Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+print(a)
+
+import sys
+print("Python version")
+print (sys.version)
+
+from datetime import date
+today = date.today()
+print("Today's date:", today)
+
+a="Hello"
+b="Mr Ibrahim,"
+c="How are you doing today"
+print(a,b,c)
+
+x=float(input("Enter the radius:"))
+y= 3.142* x**2
+print("The area of the circle is",y)
